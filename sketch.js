2049cc02f843d03064eafded1483b4bbb27f579d
Samadrita13 <77491500+Samadrita13@users.noreply.github.com>
2021-01-15 17:44:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,141 @@
+
+//Game States
+var PLAY=1;
+var END=0;
+var gameState=1;
+
+var sword,fruit ,monster,fruitGroup,enemyGroup, score,r,randomFruit;
+var swordImage , fruit1, fruit2 ,fruit3,fruit4, monsterImage, gameOverImage , swoosh , gameoversound;
+
+
+function preload(){
+  
+  swordImage = loadImage("sword.png");
+  monsterImage = loadAnimation("alien1.png","alien2.png")
+  fruit1 = loadImage("fruit1.png");
+  fruit2 = loadImage("fruit2.png");
+  fruit3 = loadImage("fruit3.png");
+  fruit4 = loadImage("fruit4.png");
+  gameOverImage = loadImage("gameOver.png")
+  gameoversound = loadSound("gameOver.mp3");
+ swoosh = loadSound ("Sound.mp3");
+}
+
+
+
+function setup() {
+  createCanvas(600, 600);
+  
+  //creating sword
+   sword=createSprite(40,200,20,20);
+   sword.addImage(swordImage);
+   sword.scale=0.7
+  
+  
+  //set collider for sword
+  sword.setCollider("rectangle",0,0,40,40);
+
+  // Score variables and Groups
+  score=0;
+  fruitGroup=createGroup();
+  enemyGroup=createGroup();
+
+  console.log("this is ",gameState)
+}
+
+function draw() {
+  background("lightblue");
+  
+  if(gameState===PLAY){
+    
+    //Call fruits and Enemy function
+    fruits();
+    Enemy();
+    
+    // Move sword with mouse
+    sword.y=World.mouseY;
+    sword.x=World.mouseX;
+  
+    // Increase score if sword touching fruit
+    if(fruitGroup.isTouching(sword)){
+      fruitGroup.destroyEach();
+      swoosh.play();
+      score=score+2;
+    }
+    else
+    {
+      // Go to end state if sword touching enemy
+      if(enemyGroup.isTouching(sword)){
+        gameState=END;
+        gameoversound.play();
+      
+        fruitGroup.destroyEach();
+        enemyGroup.destroyEach();
+        fruitGroup.setVelocityXEach(0);
+        enemyGroup.setVelocityXEach(0);
+        
+        // Change the animation of sword to gameover and reset its position
+        sword.addImage(gameOverImage);
+        sword.x=270;
+        sword.y=200;
+        sword.scale = 2;
+          text ("press 'R' to restart",200,300);
+        reset();
+      }
+     
+        
+      
+    }
+  }
+  
+  drawSprites();
+  
+  fill ("white");
+  text("Score : "+ score,300,30);
+}
+function reset(){
+if (keyDown("r")){
+gameState = PLAY;
+fruitGroup.destroyEach();
+enemyGroup.destroyEach();
+  score = 0;
+sword.changeAnimation(swordImage)  ;
+}  
+}
+
+function Enemy(){
+  if(World.frameCount%200===0){
+    monster=createSprite(100,200,20,20);
+    monster.addAnimation("moving", monsterImage);
+    monster.y=Math.round(random(100,300));
+    monster.velocityX=(8+(score/10));
+    monster.setLifetime=50;
+    
+    enemyGroup.add(monster);
+  }
+}
+
+function fruits(){
+  if(World.frameCount%80===0){
+    fruit=createSprite(500,200,20,20);
+    fruit.scale=0.2;
+     //fruit.debug=true;
+     r=Math.round(random(1,4));
+    if (r == 1) {
+      fruit.addImage(fruit1);
+    } else if (r == 2) {
+      fruit.addImage(fruit2);
+    } else if (r == 3) {
+      fruit.addImage(fruit3);
+    } else {
+      fruit.addImage(fruit4);
+    }
+    
+    fruit.y=Math.round(random(50,340));
+   
+    fruit.velocityX=-(7+(score/4));
+    fruit.setLifetime=100;
+    
+    fruitGroup.add(fruit);
+  }
+}
